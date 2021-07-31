87e1b176bcde363e16b5c420b25931e38be6e16a
Lubos Verzich <lve@paymorrow.com>
2021-07-31 20:48:08 +0200

    Added redis pubsub

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,24 @@
 import {NestFactory} from '@nestjs/core'
 import {Modules} from 'Modules'
 
+import {createClient} from "redis"
+
+global.publisher = createClient()
+global.subscriber = createClient()
+
+global.subscriber.on("connect", () => {
+	console.log("Connected to redis successfully")
+	global.subscriber.subscribe("redis-queue")
+})
+
+global.subscriber.on("message", (channel, message) => {
+	console.log("Data from " + channel + " with message: " + message)
+})
+
+
 async function bootstrap() {
-  const app = await NestFactory.create(Modules)
-  await app.listen(3000)
+	const app = await NestFactory.create(Modules)
+	await app.listen(3000)
 }
 
 bootstrap()
--- src/person/PersonController.ts
+++ src/person/PersonController.ts
@@ -3,12 +3,29 @@ import {PersonService} from 'person/PersonService'
 
 @Controller()
 export class PersonController {
-  constructor(private readonly service: PersonService) {
-  }
+	constructor(private readonly service: PersonService) {
+	}
 
-  @Get('cost')
-  getCost(): number {
-    console.log('Hello cost')
-    return this.service.getCost()
-  }
+	@Get('cost')
+	getCost(): number {
+		console.log('Hello cost')
+		return this.service.getCost()
+	}
+
+	@Get('redis')
+	publishToRedis(): string {
+
+		debugger;
+		const crypto = require('crypto')
+
+		// getting a random RFC 4122 Version 4 UUID
+		// by using randomUUID() method
+		const value = crypto.randomBytes(20).toString('hex')
+
+		console.log('Publishing to redis value: ' + value)
+
+		global.publisher.publish("redis-queue", value)
+
+		return value
+	}
 }
